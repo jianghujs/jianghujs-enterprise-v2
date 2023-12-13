059a8fb168f97a83c93198decd06a7fd658819b6
geekape <861013016@qq.com>
2023-12-13 23:35:17 +0800

    打造日志和通知,待完

--- base-task/app/service/notice.js
+++ base-task/app/service/notice.js
@@ -4,14 +4,14 @@ const { BizError, errorInfoEnum } = require('../constant/error');
 const { tableEnum } = require('../constant/constant');
 const _ = require('lodash');
 const idGenerateUtil = require("@jianghujs/jianghu/app/common/idGenerateUtil");
-
+const dayjs = require('dayjs')
 const actionDataScheme = Object.freeze({
   addNotice: {
     type: 'object',
     additionalProperties: true,
     required: [ 'taskMemberIdList', 'taskTitle', 'taskContent' ],
     properties: {
-      taskMemberIdList: { type: 'array', },
+      taskMemberIdList: { type: 'string', },
       taskTitle: { type: 'string', },
       taskContent: { type: 'string', },
     }
@@ -24,14 +24,36 @@ class NoticeService extends Service {
     validateUtil.validate(actionDataScheme.addNotice, actionData);
 
     const { jianghuKnex, knex } = this.app;
-    const { taskMemberIdList, taskTitle, taskContent } = actionData;
+    const { username } = this.ctx.userInfo;
+    let { taskMemberIdList, taskTitle, taskContent, taskType } = actionData;
 
+    taskMemberIdList = taskMemberIdList.split(',')
     let idSequence = await idGenerateUtil.idPlus({
       knex,
       tableName: 'task',
       columnName: 'idSequence',
     })
     idSequence--;
+
+    // 判断taskType类型
+    switch(taskType) {
+      case '任务':
+        taskContent = `${username}邀请您参与<a href="">《${taskTitle}》</a>任务，请及时查看`;
+        taskTitle = '任务邀请提醒';
+        break;
+      case '审批':
+        taskContent = `${username}邀请您参与<a href="">《${taskTitle}》</a>审批，请及时查看`;
+        taskTitle = '审批邀请提醒';
+        break;
+      case '日志':
+        taskContent = `${username}邀请您参与<a href="">《${taskTitle}》</a>日志，请及时查看`;
+        taskTitle = '日志邀请提醒';
+        break;
+      default:
+        taskContent = `有一个新公告 《${taskTitle}》 ，请及时查看，请及时查看`;
+        taskTitle = '公告提醒';
+        break;
+    }
 
     const insertData = taskMemberIdList.map(item=> {
 
@@ -42,11 +64,12 @@ class NoticeService extends Service {
         taskManagerId: item,
         idSequence,
         taskType: '通知',
+        taskCreateAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
         taskId: `TZ${idSequence}`,
       }
     })
    
-    await jianghuKnex(tableEnum.task).insert(insertData)
+    await jianghuKnex(tableEnum.task).jhInsert(insertData)
   }
 
   // 更新所有未读消息为【已读】
