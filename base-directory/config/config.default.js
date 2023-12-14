7b753233227e84cb201525415f7101bddcb95aec
xueyuan <xueyuan921@163.com>
2023-12-14 15:34:15 +0800

    取消jianghuId 合并

--- base-directory/config/config.default.js
+++ base-directory/config/config.default.js
@@ -28,35 +28,6 @@ module.exports = appInfo => {
 
     primaryColor: "#4caf50",
     primaryColorA80: "#EEF7EE",
-
-    jianghuConfig: {
-      jhIdConfig: {
-        // 是否开启
-        enable: true,
-        // 当前应用使用的 jhId，在使用配置表时，自动用该 jhId 做过滤
-        jhId: 'directory',
-        // 使用到 jhId 的配置表，一般保持默认即可
-        careTableViewList: [
-          '_cache',
-          '_constant',
-          '_constant_ui',
-          '_file',
-          '_group',
-          '_page',
-          '_record_history',
-          '_resource',
-          '_resource_request_log',
-          '_role',
-          '_test_case',
-          '_user',
-          '_user_group_role',
-          '_user_group_role_page',
-          '_user_group_role_resource',
-          '_user_session',
-          '_view01_user',
-        ],
-      },
-    },
     
     static: {
       dynamic: true,
