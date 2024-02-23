64b50a7e14c091e21e5cbfd8730562dfd6621cb7
fosion06-mega <fosion06-mega@jianghujs.org>
2024-02-23 16:54:35 +0800

    企微登陆

--- base-directory/config/config.local.example.js
+++ base-directory/config/config.local.example.js
@@ -20,6 +20,39 @@ module.exports = appInfo => {
         return `[${meta.date}] [${meta.level}] [${meta.ctx.method} ${meta.ctx.url}] ${meta.message}`;
       },
     },
+    wechat: {
+      devMode: true,
+      // 微信配置（公众号、小程序、开放平台）
+      apiConfig: {
+        // appId: 'appId',
+        // appScrect: 'appScrect',
+        // token: 'token',
+        // encryptMessage: true,
+        // encodingAesKey: 'encodingAesKey'
+      },
+      // 企业微信配置（应用、小程序、第三方）
+      qyApiConfig: {
+        // appId: '123',
+        // appScrect: '123',
+        // token: 'token',
+        // encryptMessage: true,
+        // encodingAesKey: 'encodingAesKey',
+        // corpId: '123'
+      },
+      // 微信支付配置
+      wxPayApiConfig: {
+        //     apiKey: 'apiKey',
+        //     apiKey3: 'apiKey3',
+        //     domain: 'domain',
+        //     appId: '应用ID',
+        //     mchId: '商户号',
+        //     certP12Path: 'cert.p12',
+        //     keyPath: 'key.pem',
+        //     certPath: 'cert.pem',
+        //     providerAppId: '服务商appId',
+        //     provideMchId: '服务商商户号'
+      },
+    },
     knex: {
       client: {
         dialect: 'mysql',
