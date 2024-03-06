b97425255e3906f3314035f25bd5f1e7af60bdda
exabytes <exabytes>
2024-03-06 18:30:07 +0800

    用户管理

--- base-system/app/constant/error.js
+++ base-system/app/constant/error.js
@@ -1,6 +1,17 @@
 'use strict';
 
+class BizError extends Error {
+  constructor({ errorCode, errorReason, errorReasonSupplement }) {
+    super(JSON.stringify({ errorCode, errorReason, errorReasonSupplement }));
+    this.name = 'BizError';
+    this.errorCode = errorCode;
+    this.errorReason = errorReason;
+    this.errorReasonSupplement = errorReasonSupplement;
+  }
+}
+
 module.exports = {
+  BizError,
   errorInfoEnum: {
     user_id_exist: { errorCode: 'user_id_exist', errorReason: '用户ID已存在' },
     user_not_exist: { errorCode: 'user_not_exist', errorReason: '用户不存在' },
