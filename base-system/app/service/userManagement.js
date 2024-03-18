fa8d6095b60d5f30009ad38dcd56b1f6fd3cd730
colin <colin>
2024-03-18 20:24:29 +0800

    groupManagement.html

--- base-system/app/service/userManagement.js
+++ base-system/app/service/userManagement.js
@@ -85,6 +85,28 @@ class UserManagementService extends Service {
     return {};
   }
 
+
+  async batchAddUser() {
+    const { jianghuKnex } = this.app;
+    const actionData = this.ctx.request.body.appData.actionData;
+    const { userList=[] } = actionData;
+
+    const userListInsert = [];
+    userList.forEach(user => {
+      const {userId, username } = user;
+      if (!userId || !username) { return; }
+      const md5Salt = idGenerateUtil.randomString(12);
+      const clearTextPassword = idGenerateUtil.randomString(6);
+      const password = md5(`${clearTextPassword}_${md5Salt}`);
+      userListInsert.push({ userId, username, clearTextPassword, md5Salt, password });
+    })
+    if (userListInsert.length > 0) {
+      await jianghuKnex('_user', this.ctx).jhInsert(userListInsert);
+    }
+    await this.service.app.buildUserApp();
+    return {};
+  }
+
   async resetUserPassword() {
     const { jianghuKnex } = this.app;
     const actionData = this.ctx.request.body.appData.actionData;
