8421013e3e4fe6d9e6913b4617bbee4a5515546e
xueyuan <xueyuan921@163.com>
2024-10-23 12:03:32 +0800

    local config 文件补充

---
+++ base-directory/config/config.local.example.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = appInfo => {
+
+  return {
+    appType: 'multiApp',
+    appDirectoryLink: '/directory',
+    authTokenKey: 'directory',
+    loginPage: '/directory/page/login',
+    wechat: {
+      qyApiConfig: {
+        // appId: '123',
+        // appScrect: '123',
+        // corpId: '123',
+        // loginRedirectUri:'https://wxsg.foisongroup.com/directory/page/login'
+      },
+    },
+    jiangHuConfig: {
+      packageIdCheck: true,
+      updateRequestDemoAndResponseDemo: false,
+    },
+    knex: {
+      client: {
+        dialect: 'mysql',
+        connection: {
+          host: '127.0.0.1',
+          port: '3306',
+          user: 'root',
+          password: '123456',
+          database: 'jh_enterprise_v2_directory',
+        },
+        pool: { min: 0, max: 10 },
+        acquireConnectionTimeout: 30000,
+      },
+      app: true,
+    },
+    logger: {
+      outputJSON: true,
+      level: 'INFO',
+      // level: 'DEBUG',
+      // allowDebugAtProd: true,
+      dir: path.join(appInfo.baseDir, 'logs'),
+      contextFormatter(meta) {
+        return `[${meta.date}] [${meta.level}] [${meta.ctx.method} ${meta.ctx.url}] ${meta.message}`;
+      },
+    },
+  };
+};
---
+++ base-system/config/config.local.example.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = appInfo => {
+
+  return {
+    appType: 'multiApp',
+    appDirectoryLink: '/directory',
+    authTokenKey: 'directory',
+    loginPage: '/directory/page/login',
+    jiangHuConfig: {
+      packageIdCheck: false,
+      updateRequestDemoAndResponseDemo: false,
+      enableUserInfoCache: true,
+      userInfoCacheRefreshInterval: '10s',
+    },
+    logger: {
+      outputJSON: true,
+      level: 'INFO',
+      // level: 'DEBUG',
+      // allowDebugAtProd: true,
+      dir: path.join(appInfo.baseDir, 'logs'),
+      contextFormatter(meta) {
+        return `[${meta.date}] [${meta.level}] [${meta.ctx.method} ${meta.ctx.url}] ${meta.message}`;
+      },
+    },
+    knex: {
+      client: {
+        dialect: 'mysql',
+        connection: {
+          host: '127.0.0.1',
+          port: '3306',
+          user: 'root',
+          password: '123456',
+          database: 'jh_enterprise_v2_system',
+        },
+        pool: { min: 0, max: 10 },
+        acquireConnectionTimeout: 30000,
+      },
+      app: true,
+    },
+  };
+};
